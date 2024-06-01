0fc797b907f5ee7332b7bc737cba75d868a533ad
Cope01 <petarurosevic4@gmail.com>
2024-06-01 16:34:49 +0200

    Update show-all-products.blade.php
    Added background color to cards on home page

--- resources/views/product/show-all-products.blade.php
+++ resources/views/product/show-all-products.blade.php
@@ -21,6 +21,7 @@
         .card {
             border: none;
             transition: all 0.3s ease;
+            background-color: #f2eef2;
         }
         .card:hover {
             box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
