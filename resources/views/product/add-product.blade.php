1e379fb1b9e46ff0ca1f3377806c46ebbc1b394f
Cope01 <petarurosevic4@gmail.com>
2024-06-02 14:42:08 +0200

    Update add-product.blade.php
    Changed the look of the page, changed the colors, changed the position of buttons and text, added different looks to button and card, deleted comments.

--- resources/views/product/add-product.blade.php
+++ resources/views/product/add-product.blade.php
@@ -2,20 +2,35 @@
 <html lang="sr">
 <head>
     @include('home.navbar')
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Forma</title>
+    <style>
+         .submit {
+            border: none;
+            outline: none;
+            padding: 8px 30px;
+            border-radius: 5px;
+            color: #fff !important;
+            font-size: 16px;
+            transform: .8s ease;
+            background-color: #b22d64 !important;
+        }
+        .submit:hover {
+            background-color: #512940 !important;
+            transition: .8s ease;
+            transform: scale(1.05);
+        }
+        </style>
 </head>
 <body>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header text-white" style="background-color:#17a2b8;">
-                        <h4 style="margin-left: 42%">Dodaj stavku</h4>
+                    <div class="card-header text-white" style="background-color:#b22d64; text-align: center;">
+                        <h4>Dodaj stavku</h4>
                     </div>
                     <div class="card-body">
                         <form action="/add-product" method="post" enctype="multipart/form-data">
@@ -41,16 +56,13 @@
                                 <label for="image" class="form-label">Slika</label>
                                 <input type="file" id="image" name="image" class="form-control" required>
                             </div>
-                            
-                            <button type="submit" class="btn btn-outline-dark" style="margin-left: 45%;">Potvrdi</button>
+                            <button type="submit" class="btn submit" style="margin-left:40%">Potvrdi</button>                        
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap JS (optional) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 @include('footer')
